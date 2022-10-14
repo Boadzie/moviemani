64f98b1bd2af29540b971fd781c14c2c47ff5bd0
Daniel Boadzie <boadziedaniel43@gmail.com>
2022-10-14 09:54:47 +0000

    Add movie_list view, url and template
    Add support for image upload

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Movie
+
+
+class MovieListView(ListView):
+    model = Movie
+    context_object_name = "movies"
+    template_name = "movie_list.html"
