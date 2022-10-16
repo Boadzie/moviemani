7ca7b7fc48f1eb3a3a07f649d8c14ec6083785f3
Daniel Boadzie <boadziedaniel43@gmail.com>
2022-10-16 14:33:48 +0000

    Add account views to app

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from .forms import CustomUserCreationForm, LoginForm
+
+
+class LoginView(auth_views.LoginView):
+    form_class = LoginForm
+    template_name = "registration/login.html"
+
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
